9d84e7431c314ff231a4033bbc0080783a485f5d
Fabricio Fernandes <clf.fabricio@gmail.com>
2022-07-08 16:48:57 -0300

    jump

--- js/script.js
+++ js/script.js
@@ -5,15 +5,6 @@ const coin = document.querySelector('.coin');
 const coin2 = document.querySelector('.coin2');
 const coin3 = document.querySelector('.coin3');
 
-
-const jump = () => {
-    mario.classList.add('jump');
-
-    setTimeout(() =>{
-        mario.classList.remove('jump');
-    }, 500);
-}
-
 const score = setInterval(() =>{
     const coinPosition = coin.offsetLeft;
     const coin2Position = coin2.offsetLeft;
@@ -22,7 +13,7 @@ const score = setInterval(() =>{
 
     if(marioPosition >= 60 && coinPosition <= 120){
         coin.style.visibility = 'hidden';
-
+        var point = 1;
         setTimeout(() =>{
             coin.style.visibility = 'visible';
         }, 550);
@@ -34,8 +25,6 @@ const score = setInterval(() =>{
         setTimeout(() =>{
             coin2.style.visibility = 'visible';
         }, 570);
-        
-       
     }
 
     if(marioPosition >= 60 && coin3Position <= 120){
@@ -44,28 +33,17 @@ const score = setInterval(() =>{
         setTimeout(() =>{
             coin3.style.visibility = 'visible';
         }, 550);
-
     }
-})
-
-/*const contador = () =>{
-    let elemento = document.querySelector('.contador');
-	let contador = 0;
-    
-    function contador(){
-        contador + score
-    }
-		
-	setInterval(() => elemento.innerHTML = contador, 1000);
-}*/
+});
 
 const loop = setInterval(() => {
-
    const pipePosition = pipe.offsetLeft;
    const cloudsPosition = clouds.offsetLeft;
+   const coinPosition = coin.offsetLeft;
+   const coin2Position = coin2.offsetLeft;
+   const coin3Position = coin3.offsetLeft;
    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-/*
     if (pipePosition <= 120 && marioPosition <= 80 && pipePosition > 0) {
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
@@ -79,10 +57,29 @@ const loop = setInterval(() => {
         clouds.style.animation = 'none';
         clouds.style.left = `${cloudsPosition}px`;
 
+        coin.style.animation = 'none';
+        coin.style.left = `${coinPosition}px`;
+        coin2.style.animation = 'none';
+        coin2.style.left = `${coin2Position}px`;
+        coin3.style.animation = 'none';
+        coin3.style.left = `${coin3Position}px`;
+
      //   alert("Game Over");
 
         clearInterval(loop);
-   }*/
+   }
 }, 10);
   
-document.addEventListener('keydown', jump);
+//document.addEventListener('keydown', jump);
+
+document.addEventListener('keydown', function(movimento) {
+    if (movimento.key === "ArrowUp" || movimento.keyCode === 32){
+        mario.classList.add('jump');
+        setTimeout(() =>{
+            mario.classList.remove('jump');
+        }, 500);
+    } else {
+        
+
+    }
+});
